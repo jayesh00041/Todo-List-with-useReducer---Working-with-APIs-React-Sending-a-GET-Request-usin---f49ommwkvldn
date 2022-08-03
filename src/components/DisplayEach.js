2c4c1d4c0c35e6e66b4a182c4012f1aec41920e0
jayesh00041 <jayesh00041@gmail.com>
2022-08-03 20:36:56 +0530

    Update DisplayEach.js

--- src/components/DisplayEach.js
+++ src/components/DisplayEach.js
@@ -6,8 +6,7 @@ export default function DisplayEach({value, handleBtn}) {
   return (
     <div>
         <div className="todo-title" style={value.isDone?{color:'grey'}:{}}>{value.todo_name}</div>
-        <button onClick = {()=>handleBtn({type: ACTION.DONE, payload: value.id})}>Completed</button>
-        <button className='todo-delete' onClick = {()=>handleBtn({type: ACTION.DELETE, payload: value.id})}>Delete</button>
+          <button className='todo-delete' onClick = {()=>handleBtn({type: ACTION.DELETE, payload: value.id})}>Delete</button>
     </div>
   )
 }
